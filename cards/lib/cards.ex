6d9364b477afd5259b433207813d89f772379361
yaoandrew <yaoandrew@gmail.com>
2017-06-03 05:47:22 -0700

    Clean up whitespace

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,19 +1,19 @@
 defmodule Cards do
 
   def create_deck do
-   values = ["Ace", "Two", "Three", "Four", "Five"] 
+   values = ["Ace", "Two", "Three", "Four", "Five"]
    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-   for suit <- suits, value <- values do 
+   for suit <- suits, value <- values do
        "#{value} of #{suit}"
     end
   end
-  
+
   def shuffle(deck) do
-   Enum.shuffle(deck) 
-  end 
- 
+   Enum.shuffle(deck)
+  end
+
   def contains?(deck, card) do
-   Enum.member?(deck, card) 
+   Enum.member?(deck, card)
   end
 
   def deal(deck, num_of_cards) do
