7f3fa0ba5a24b4b83d9ad1878018847966c70d0e
Andrew J Yao <yaoandrew@gmail.com>
2017-06-05 21:44:38 -0700

    Case test section 29

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,20 +1,51 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
 
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
    values = ["Ace", "Two", "Three", "Four", "Five"]
    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
    for suit <- suits, value <- values do
        "#{value} of #{suit}"
-    end
+   end
   end
 
   def shuffle(deck) do
    Enum.shuffle(deck)
   end
+  @doc """
+    Determines if a deck contains a given card
 
+    ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
+   """
   def contains?(deck, card) do
    Enum.member?(deck, card)
   end
+
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
 
   def deal(deck, num_of_cards) do
     Enum.split(deck, num_of_cards)
